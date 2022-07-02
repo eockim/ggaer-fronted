6b6ec20fa23995c464a50b2bb7992ef2ad52042a
eockim <hyungjoon6876@gmail.com>
2022-07-02 10:23:53 +0900

    init

---
+++ pages/ggaer.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div>Hello GGaeR</div>
+        <NuxtLink to="/">Home page</NuxtLink>
+        <span> 
+            <v-badge
+            bottom
+            inline
+            tile
+            content="1a">
+                <v-icon
+      large
+      color="teal darken-2"
+    >
+      
+    </v-icon></v-badge>
+        </span>
+        <span>
+            <v-alert type="success">
+                aaaa
+            </v-alert>
+        </span>
+        <span>
+            <font-awesome-icon icon="fa-solid fa-anchor" />
+            
+        </span>
+        <span>
+            <font-awesome-icon icon="fa-solid fa-cog" />
+        </span>
+        <span>
+        <font-awesome-icon icon="fa-brands fa-slack" />
+        </span>
+                <span>
+        <font-awesome-icon icon="fa-regular fa-user" />
+        </span>
+
+    </div>
+</template>
+
